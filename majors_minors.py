7a0e1e6041595bd06a1f63b17cfcf514566c3f13
Anderson Adon <andersonadon@gmail.com>
2019-09-14 20:49:50 -0400

    now sorted by how classes left, only shows top three minors

--- majors_minors.py
+++ majors_minors.py
@@ -6,15 +6,35 @@ from physicsminor import check_physics_minor
 from cisminor import check_cis_minor
 from get_courses import generate_courses_file
 
+def print_required_courses(courses_left):
+    pass
+
+# Update json file with all courses if there have been some changes
 option = input("Update courses listings? (Y/N): ")
 if option == 'Y':
     generate_courses_file()
 
+# Parse file to get list of courses student has taken
 course_file_name = input("Enter course file name: ")
 course_file_path = Path(course_file_name)
 course_file = open(course_file_path)
 user_courses = course_file.readlines()
 user_courses = [course.strip() for course in user_courses]
+
+# List of tuples, where first item is the department, second item is courses needed
+minors = []
+minors.append(('Computer Science', check_cs_minor(user_courses)))
+minors.append(('Africana Studies', check_as_minor(user_courses)))
+minors.append(('Anthropolgy', check_anthro_minor(user_courses)))
+minors.append(('Physics', check_physics_minor(user_courses)))
+minors.append(('Computer Integrated Surgery', check_cis_minor(user_courses)))
+
+# Sort list of possible minors by number of courses needed
+minors = sorted(minors, key=lambda tup : len(tup[1]))
+
+for i in range(3):
+    print(f'You are {len(minors[i][1])} course(s) away from a {minors[i][0]} minor!')
+
 
 cs_minor_courses_left = check_cs_minor(user_courses)
 as_minor_courses_left = check_as_minor(user_courses)
